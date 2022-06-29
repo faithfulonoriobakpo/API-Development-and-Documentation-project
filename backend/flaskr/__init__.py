0258470080a4499a33eca3c6aac867d1bc62d6ab
faithfulonoriobakpo <faithful.onoriobakpo@gmail.com>
2022-06-29 20:31:26 +0100

    set access control allow

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -22,14 +22,18 @@ def create_app(test_config=None):
     setup_db(app)
 
     """
-    @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
+    Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
     """
     CORS(app, resources={'/': {'origins': '*'}})
 
     """
-    @TODO: Use the after_request decorator to set Access-Control-Allow
+    Use the after_request decorator to set Access-Control-Allow
     """
-
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization, true')
+    response.headers.add('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS')
+    return response
     """
     @TODO:
     Create an endpoint to handle GET requests
